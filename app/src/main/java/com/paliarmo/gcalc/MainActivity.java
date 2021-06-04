02c67fce9eb0b01077a66408c8f79eb469bbad97
PaLiarMo <paliarmo@gmail.com>
2021-06-04 23:08:48 +0300

    init commit

---
+++ app/src/main/java/com/paliarmo/gcalc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.paliarmo.gcalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
