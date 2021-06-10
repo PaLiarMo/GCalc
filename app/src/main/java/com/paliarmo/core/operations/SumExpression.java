673f8c84352d51b099966ba7cf0a7812aa1b1762
PaLiarMo <paliarmo@gmail.com>
2021-06-10 16:12:51 +0300

    single log tag

--- app/src/main/java/com/paliarmo/core/operations/SumExpression.java
+++ app/src/main/java/com/paliarmo/core/operations/SumExpression.java
@@ -10,17 +10,18 @@ public class SumExpression implements OperationMathExpression {
     private MathExpression a;
     private MathExpression b;
 
+    private String LOG = "SumExpression";
     private NumberExpression result;
 
     @Override
     public void addOperand(MathExpression operand) {
-        Log.d("SumExpression:", "addOperand");
+        Log.d(LOG, "addOperand");
         if (a == null){
             a = operand;
-            Log.d("SumExpression:", "a = "+a.calculate().getValue());
+            Log.d(LOG, "a = "+a.calculate().getValue());
         }else if (b == null){
             b = operand;
-            Log.d("SumExpression:", "b = "+a.calculate().getValue());
+            Log.d(LOG, "b = "+a.calculate().getValue());
         }
     }
 
@@ -39,24 +40,24 @@ public class SumExpression implements OperationMathExpression {
 
     @Override
     public NumberExpression calculate() {
-        Log.d("SumExpression:", "calculate");
+        Log.d(LOG, "calculate");
         if (a.calculate().getValue() instanceof Double || b.calculate().getValue() instanceof Double){
-            Log.d("SumExpression:", "work with double");
+            Log.d(LOG, "work with double");
             Double first = a.calculate().getValue() instanceof Double ? (Double) a.calculate().getValue() : Double.valueOf((Integer) a.calculate().getValue());
             Double second = b.calculate().getValue() instanceof Double ? (Double) b.calculate().getValue() : Double.valueOf((Integer) b.calculate().getValue());
             Double newValue = first + second;
-            Log.d("SumExpression:", "first = "+ first);
-            Log.d("SumExpression:", "second = "+ second);
-            Log.d("SumExpression:", "sum = "+ newValue);
+            Log.d(LOG, "first = "+ first);
+            Log.d(LOG, "second = "+ second);
+            Log.d(LOG, "sum = "+ newValue);
             result = new NumberExpression(newValue);
         }else{
-            Log.d("SumExpression:", "work with integer");
+            Log.d(LOG, "work with integer");
             Integer first = (Integer) a.calculate().getValue();
             Integer second = (Integer) b.calculate().getValue();
             Integer newValue = first + second;
-            Log.d("SumExpression:", "first = "+ first);
-            Log.d("SumExpression:", "second = "+ second);
-            Log.d("SumExpression:", "sum = "+ newValue);
+            Log.d(LOG, "first = "+ first);
+            Log.d(LOG, "second = "+ second);
+            Log.d(LOG, "sum = "+ newValue);
             result = new NumberExpression(newValue);
         }
 
